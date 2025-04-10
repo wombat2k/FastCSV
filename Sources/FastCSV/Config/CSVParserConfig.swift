74bf5f1df274c51d519a53afb2817908ef9d8c26
Patrick Abeya <wombat@marsupial.org>
2025-04-10 10:23:50 -0400

    Added an assume no quote setting.

--- Sources/FastCSV/Config/CSVParserConfig.swift
+++ Sources/FastCSV/Config/CSVParserConfig.swift
@@ -8,6 +8,9 @@ public struct CSVParserConfig {
     /// Size of the buffer used when reading from disk (in bytes)
     public let readBufferSize: Int
 
+    /// Whether to assume that the CSV file does not contain any quoted fields
+    public let assumeNoQuotes: Bool
+
     /// Initialize a new CSV parser configuration
     /// - Parameters:
     ///   - delimiter: CSV delimiters (field, row, quote) (default: CSV format with comma, newline, double-quote)
@@ -15,8 +18,10 @@ public struct CSVParserConfig {
     public init(
         delimiter: Delimiter? = nil,
         readBufferSize: Int = 256 * 1024,
+        assumeNoQuotes: Bool = false
     ) {
         self.delimiter = delimiter ?? CSVFormat.csv.delimiter
         self.readBufferSize = readBufferSize
+        self.assumeNoQuotes = assumeNoQuotes
     }
 }
